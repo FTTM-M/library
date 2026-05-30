09d69ded9a7f55e728a0dad506c61327da292b0b
FTTM <fatemehalimirzaee14@gmail.com>
2026-05-30 13:24:43 +0330

    styled the layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,16 @@
+import styles from "./Layout.module.css";
+
 function Layout({ children }) {
   return (
     <>
-      <header>header</header>
+      <header className={styles.header}>
+        <h1>Book App</h1>
+        <p>Book is all ypu find ...</p>
+      </header>
       {children}
-      <footer>footer</footer>
+      <footer className={styles.footer}>
+        developed by <a href="https://github.com/FTTM-M/library">FTTM</a>
+      </footer>
     </>
   );
 }
