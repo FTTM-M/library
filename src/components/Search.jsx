84691a640f706539e2eb0a0556032016d2f106ab
FTTM <fatemehalimirzaee14@gmail.com>
2026-06-09 13:32:36 +0330

    styled the search box

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,11 @@
 import { FaSearch } from "react-icons/fa";
 
+import styles from "./search.module.css";
+
+
 function Search({search, Searchhandler,BookHandler}) {
   return (
-    <div>
+    <div className={styles.search}>
       <input type="text" placeholder="Search" value={search} onChange={Searchhandler} />
       <button onClick={BookHandler}>
         <FaSearch />
